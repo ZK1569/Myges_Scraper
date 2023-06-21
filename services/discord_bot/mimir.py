b539b17b0be621416e7e99e6c87097f0c394b958
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-06-21 22:26:47 +0200

    setup discord bot

---
+++ services/discord_bot/mimir.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import os 
+from dotenv import load_dotenv
+
+# Charger les variables d'environnement à partir du fichier .env
+load_dotenv()
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix='!',intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'The bot is up {client.user} ')
+    print("-"*15)
+
+@client.command()
+async def ping(ctx):
+    """Tu en demande trop """
+    await ctx.send('pong')
+
+client.run(str(os.getenv('TOKEN')))
