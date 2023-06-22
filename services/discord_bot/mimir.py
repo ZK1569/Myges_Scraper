6e50d0792b5cc23592c658506827aceb28ac4148
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-06-22 18:49:29 +0200

    command save password and mail

--- services/discord_bot/mimir.py
+++ services/discord_bot/mimir.py
@@ -23,8 +23,34 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @client.command()
-async def save(ctx):
-    await ctx.send("Functionality is not finished, come back later")
+async def save(ctx, email, password):
+
+    # Check if the user has already saved this information 
+    if db.isUserSaved:
+        await ctx.send("Your information is already saved. If you want to change your password, you can do `!changepassword <YOUR NEW PASSWORD>`")
+        return 
+    
+    # Checks that the user has sent an e-mail and password 
+    if not email or not password:
+        await ctx.send("You forgot to send me your email and password")
+        return 
+    
+    # Check if the email variable has an @ in it
+    if not "@" in email:
+        await ctx.send("I'm not strupid, this is not a valid mail ")
+
+    print("Normalement là la personne est pas connue")
+
+    user_id = ctx.author.id
+
+    isSaved = db.saveLogin(user_id, email, password)
+
+    if(isSaved):
+        await ctx.send("Thanks for the personal info ...")
+        return
+    
+    await ctx.send("I'm so stupid I couldn't save the information.")
+    return
 
 @client.command()
 async def me(ctx):
@@ -38,5 +64,10 @@ async def me(ctx):
         message += f"I don't know your password and email \nYou can do the cmd `!save <YOUR EMAIL> <PASSWORD>`"
 
     await ctx.send(message)
+
+@client.command()
+async def changepassword(ctx):
+    await ctx.send("The functionality is not finished, come back later.")
+
 
 client.run(str(os.getenv('TOKEN')))
